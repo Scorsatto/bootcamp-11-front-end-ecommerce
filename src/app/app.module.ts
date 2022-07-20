b01ef9eee92a374b873a3522f08b8e4207187de7
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2022-07-20 09:33:17 -0400

    projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { NavComponent } from './components/shared/nav/nav.component';
 import { BookstoreAppComponent } from './components/bookstore-app/bookstore-app.component';
 import { FiltersComponent } from './components/bookstore-app/filters/filters.component';
 import { ProductListComponent } from './components/bookstore-app/product-list/product-list.component';
+import { ProductItemComponent } from './components/bookstore-app/product-list/product-item/product-item.component';
+import { BooksService } from './components/bookstore-app/product-list/product-list.component.service';
 
 @NgModule({
   declarations: [
@@ -18,13 +21,15 @@ import { ProductListComponent } from './components/bookstore-app/product-list/pr
     NavComponent,
     BookstoreAppComponent,
     FiltersComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BooksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
